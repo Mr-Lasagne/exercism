5193d8332f7204e354d9c4391f2772689912b9a8
Jack <toastlinaker@rocketmail.com>
2024-02-08 19:40:42 +0000

    Remove non-exercism files

--- cs50p/week-0/indoor/indoor.py
+++
@@ -1,5 +0,0 @@
-# Take input from user and convert it to lowercase
-text = input("Type something in caps to see it converted to lowercase! ").lower()
-
-# Print converted input
-print(text)
--- cs50p/week-0/playback/playback.py
+++
@@ -1,8 +0,0 @@
-# Take input from user and remove leading and trailing whitespace
-text = input().strip()
-
-# Replace remaining whitespace with "..."
-text = text.replace(" ", "...")
-
-# Print converted input
-print(text)
